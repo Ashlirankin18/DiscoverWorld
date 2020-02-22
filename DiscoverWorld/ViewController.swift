537c9c1d5201479a68041dc6261c595ef3270d35
Ashli Rankin <ashlirankin18@gmail.com>
2020-02-22 13:17:44 -0500

    Initial Commit

---
+++ DiscoverWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiscoverWorld
+//
+//  Created by Ashli Rankin on 2/22/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
