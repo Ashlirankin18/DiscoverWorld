1a114bd88d6041b516ef8b749ee34eebbe6e4748
Ashli Rankin <ashlirankin18@gmail.com>
2020-02-22 13:29:32 -0500

    Created a country object.

---
+++ DiscoverWorld/Models/Country.swift
@@ -0,0 +1,17 @@
+//
+//  Country.swift
+//  DiscoverWorld
+//
+//  Created by Ashli Rankin on 2/22/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import Foundation
+
+struct Country: Codable {
+    let id: String
+    let name: String
+    
+}
+
+
