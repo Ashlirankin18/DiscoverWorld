a79a981a2f282d7eb301f62dd0ed2cbf8cac25dc
Ashli Rankin <ashlirankin18@gmail.com>
2020-02-22 19:17:32 -0500

    Added AttractionCollectionViewController and Cell

---
+++ DiscoverWorld/Attractions/Controllers/AttractionCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AttractionCollectionViewController.swift
+//  DiscoverWorld
+//
+//  Created by Ashli Rankin on 2/22/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+/// `UICollectionViewController` subclass which displays attractions
+final class AttractionCollectionViewController: UICollectionViewController {
+    
+}
---
+++ DiscoverWorld/Attractions/Views/AttractionCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AttractionCollectionViewCell.swift
+//  DiscoverWorld
+//
+//  Created by Ashli Rankin on 2/22/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+/// `UICollectionViewCell` subclass which displays an attraction.
+final class AttractionCollectionViewCell: UICollectionViewCell {
+}
