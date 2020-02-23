69f7b1cb88b6be7f07311505e47ff20815aa3958
Ashli Rankin <ashlirankin18@gmail.com>
2020-02-23 08:00:19 -0500

    Addeed viewModel to cells, as well as documentation.

--- DiscoverWorld/Attractions/Views/AttractionCollectionViewCell.swift
+++ DiscoverWorld/Attractions/Views/AttractionCollectionViewCell.swift
@@ -10,4 +10,32 @@ import UIKit
 
 /// `UICollectionViewCell` subclass which displays an attraction.
 final class AttractionCollectionViewCell: UICollectionViewCell {
+    
+    /// Configures the `AttractionCollectionViewCell` with data it needs to display.
+    struct ViewModel {
+        
+        /// The name of the attraction.
+        let name: String
+        
+        /// The attraction's image
+        let image: UIImage
+        
+        /// The description of the attraction.
+        let description: String
+        
+    }
+    
+    /// Single point of configuration of the `AttractionCollectionViewCell`.
+    var viewModel: ViewModel? {
+        didSet {
+            attractionImageView.image = viewModel?.image
+            attractionNameLabel.text = viewModel?.name
+            attractinDescriptionLabel.text = viewModel?.description
+        }
+    }
+    
+    @IBOutlet private weak var cellBackgroundView: UIView!
+    @IBOutlet private weak var attractionImageView: UIImageView!
+    @IBOutlet private weak var attractionNameLabel: UILabel!
+    @IBOutlet private weak var attractinDescriptionLabel: UILabel!
 }
