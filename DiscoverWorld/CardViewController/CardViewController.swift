57e217c4ae2b234e4c990437e84b541c1cd98f9e
Ashli Rankin <ashlirankin18@gmail.com>
2020-02-23 21:42:47 -0500

    Added a CardViewController to display a detail of an attraction.

--- DiscoverWorld/CardViewController/CardViewController.swift
+++ DiscoverWorld/CardViewController/CardViewController.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
-class CardViewController: UIViewController {
-
+final class CardViewController: UIViewController {
+  
+    @IBOutlet weak var handleAreaView: UIView!
+    
+    private let attraction: Attraction
+    private let imageLoader: ImageLoader
+    
+    // MARK: - CardViewController
+    
+    /// Creates a new instance of `CardViewController`.
+    /// - Parameters:
+    ///   - attraction: A country's attraction.
+    ///   - imageLoader: Loads the image from a URL.
+    init(attraction: Attraction, imageLoader: ImageLoader) {
+        self.attraction = attraction
+        self.imageLoader = imageLoader
+        super.init(nibName: "CardViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UIViewControlller
     override func viewDidLoad() {
         super.viewDidLoad()
     }
