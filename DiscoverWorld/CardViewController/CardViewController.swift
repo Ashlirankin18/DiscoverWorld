190a2321564ce5d8f69b8b62bbd1b6d9cee81660
Ashli Rankin <ashlirankin18@gmail.com>
2020-02-23 20:08:15 -0500

    Added a cardViewController

---
+++ DiscoverWorld/CardViewController/CardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CardViewController.swift
+//  DiscoverWorld
+//
+//  Created by Ashli Rankin on 2/23/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
