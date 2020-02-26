5f659976cba9206a7097c81ee54d963b81b73547
AshliRankin <ashlirankin18@gmail.com>
2020-02-25 22:54:48 -0500

    Added documentation.

--- DiscoverWorld/CardViewController/CardViewController.swift
+++ DiscoverWorld/CardViewController/CardViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+
+/// Subscribe to this object if you would like to receive notifications about the status of the pan gesture recognizer.
 protocol CardViewControllerDelegate: AnyObject {
+    
+    /// Called when a pan gesture has begun.
+    /// - Parameter cardViewController: `CardPresentationController` displays detils about an attraction.
     func panGestureDidBegin(_ cardViewController: CardViewController)
+    
+    /// Called when a pan gesture has changed
+    /// - Parameters:
+    ///   - cardViewController: `CardPresentationController` displays detils about an attraction.
+    ///   - translation: The translation of the pan gesture in the coordinate system of the specified view.
     func panGestureDidChange(_ cardViewController: CardViewController, with translation: CGPoint)
+    
+    /// Called when a pan gesture has ended
+    /// - Parameter cardViewController: `CardPresentationController` displays detils about an attraction.
     func panGestureDidEnd(_ cardViewController: CardViewController)
 }
+
 /// `UIViewController` subclass which displays details about an attraction.
 final class CardViewController: UIViewController {
     
@@ -22,6 +36,7 @@ final class CardViewController: UIViewController {
     
     private lazy var panGestureRecognizer: UIPanGestureRecognizer = .init(target: self, action: #selector(handleCardPan(panGestureRecognizer:)))
     
+    /// Notifies subscriber to pan gesture recognizer changes.
     weak var delegate: CardViewControllerDelegate?
     
     // MARK: - CardViewController
